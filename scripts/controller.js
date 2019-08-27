2700036e1fc6f3b12472887c846e5dee15598a6c
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-08-27 21:20:43 +0800

    Included advance functionality as comments

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,4 +1,4 @@
-var client;
+// basic functionalities
 client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
 client.subscribe("mqtt/demo")
 
@@ -7,7 +7,35 @@ client.on("connect", function(){
 })
 
 client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "))
-  client.end()
+  console.log([topic, payload].join(": "));
+  client.end();
 })
+
 client.publish("mqtt/demo", "hello world!")
+
+// // advance functionalities
+// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+// client.subscribe("mqtt/demo", function (err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("subscribed")
+//   }
+// })
+
+// client.on("connect", function(){
+//     console.log("Successfully connected");
+// })
+
+// client.on("message", function (topic, payload) {
+//   console.log([topic, payload].join(": "));
+//   client.end();
+// })
+
+// client.publish("mqtt/demo", "hello world!", function(err){
+//   if (err){
+//     console.log(err)
+//   } else {
+//     console.log("published")
+//   }
+// })
