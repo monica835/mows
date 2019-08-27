26aa4fc17af671a698fcd22d35f608d6d3c8ba2f
junreycloudstrife <junrey.cloudstrife@gmail.com>
2019-08-27 20:43:21 +0800

    Moved browserMqtt.js to scripts directory

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,8 +1,13 @@
-var client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+var client;
+client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
 client.subscribe("mqtt/demo")
 
+client.on("connect", function(){
+    console.log("Successfully connected");
+})
+
 client.on("message", function (topic, payload) {
-  alert([topic, payload].join(": "))
+  console.log([topic, payload].join(": "))
   client.end()
 })
 client.publish("mqtt/demo", "hello world!")
