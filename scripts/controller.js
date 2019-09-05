aa64fb23a3d40472e9feb986b50131feb489a07b
monica835 <54056666+monica835@users.noreply.github.com>
2019-09-05 21:08:24 +0800

    Add files via upload

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,41 +1,134 @@
-// basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+$(document).ready(function () { })
+$('#btn-connect').click(function () {
+	client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
+	client.subscribe($("#topic").val());
+	console.log('connect button when clicked');
+	$("#status").text("Connecting...");
+	$("#status").css("background-color", "rgb(230, 230, 0)");
+	client.on("connect", function () {
+		$("#status").text(" connected!!!!");
+		Swal.fire(
+			'Success!!',
+			'Youre connected to the broker!'
 
-client.on("connect", function(){
-    console.log("Successfully connected");
-})
+		)
+		console.log("success");
+	});
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
+	$("#btn-disconnect").click(function () {
+		Swal.fire({
+			title: 'Are you sure?',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Yes!'
+		}).then((result) => {
+			if (result.value) {
+				client.end();
+				Swal.fire(
+					'Disconnected!',
+					'Your are disconnected to the broker.',
+					'success'
+				);
+				$("#status").text("Disconnected");
+			}
+		})
 
-client.publish("mqtt/demo", "hello world!")
+	});
 
-// // advance functionalities
-// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-// client.subscribe("mqtt/demo", function (err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("subscribed")
-//   }
-// })
+	$("#btn-publish").click(function () {
+		var topic = $("#topic").val();
+		var payload = $("#message").val();
+		if (topic == "" && payload == "") {
+			Swal.fire({
+				type: 'error',
+				title: 'Oops...',
+				text: 'Please provide inputs!',
+			});
+		}
+		else {
+			client.publish(topic, payload, function (err) {
+				if (err) {
+					Swal.fire({
+						type: 'error',
+						title: 'Oops...',
+						text: 'There is an error!',
+					});
+				} else {
+					console.log("published")
+					Swal.fire(' topic has been published successfully!')
+					var row = $("<tr>");
+					$("<td>").text(topic).appendTo($(row));
+					$("<td>").text(payload).appendTo($(row));
+					$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+					$("#tbl-body-publish").append($(row));
 
-// client.on("connect", function(){
-//     console.log("Successfully connected");
-// })
+				}
+			});
 
-// client.on("message", function (topic, payload) {
-//   console.log([topic, payload].join(": "));
-//   client.end();
-// })
 
-// client.publish("mqtt/demo", "hello world!", function(err){
-//   if (err){
-//     console.log(err)
-//   } else {
-//     console.log("published")
-//   }
-// })
+		}
+
+	})
+	$("#btn-subscribe").click(function () {
+		var topic = $("#topic").val();
+		var subscribe = $("#topic-sub").val();
+		if (subscribe != topic) {
+			Swal.fire({
+				type: 'error',
+				title: 'Oops...',
+				text: 'Topic is not available!',
+			});
+		}
+		else if (subscribe == topic && topic !== "") {
+			client.subscribe(topic, function (err) {
+				if (err) {
+					Swal.fire({
+						type: 'error',
+						title: 'Oops...',
+						text: 'An error occurs!',
+					});
+				} else {
+					var row = $("<tr>").attr("id", "mysub");
+					$("<td>").text(topic).appendTo($(row));
+					$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+					$("#tbl-body-subscribe").append($(row));
+					Swal.fire('Subscribed successfully!');
+				}
+			});
+
+		}
+
+	})
+	$("#btn-unsubsribe").click(function () {
+		var topic = $("#topic").val();
+		client.unsubscribe(topic, function (err) {
+			if (err) {
+				Swal.fire({
+					type: 'error',
+					title: 'Oops...',
+					text: 'An error occurs!',
+				});
+			} else {
+				swal.fire({
+					title: 'Confirm',
+					text: 'Are you sure you want to unsubscribe?',
+					type: 'warning',
+					showCancelButton: true,
+					confirmButtonColor: 'green'
+				});
+			}
+		});
+	})
+
+	client.on("message", function (topic, payload) {
+		// console.log([topic, payload].join(": "));
+		var row = $("<tr>");
+		$("<td>").text(topic).appendTo($(row));
+		$("<td>").text(payload).appendTo($(row));
+		$("<td>").text(moment().format('MMMM Do YYYY, h:mm:ss a')).appendTo($(row));
+		$("#tbl-body").append($(row));
+	})
+});
+
